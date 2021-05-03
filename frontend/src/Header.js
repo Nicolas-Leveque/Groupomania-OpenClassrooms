7a4e71b2c00f7c2420abacf4e8496e57419ef913
Nicolas Leveque <nicoleveque@outlook.com>
2021-05-03 11:59:43 +0200

    frontend: ajout des fichiers logos, creation du composant Header

---
+++ frontend/src/Header.js
@@ -0,0 +1,13 @@
+import './Header.css'
+import logo from './logos/icon-left-font-monochrome-white.svg'
+const Header = () => {
+    return (
+        <header>
+            <img className="logo" src={logo} alt='groupomania logo' />
+            <a href="#profil" >Profil</a>
+
+        </header>
+    )
+}
+
+export default Header
