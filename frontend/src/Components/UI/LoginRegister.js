9e973d09acd2c84783dbd1e3b8f4308d7380ce92
Nicolas Leveque <nicoleveque@outlook.com>
2021-06-08 21:46:42 +0200

    Refactorisation de LoginRegister en composant fonctionnel

--- frontend/src/Components/UI/LoginRegister.js
+++ frontend/src/Components/UI/LoginRegister.js
@@ -1,49 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LoginBox from "./LoginBox"
 import RegisterBox from "./RegisterBox"
 import "./LoginRegister.css"
 
-class LoginRegister extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { isLoginOpen: true, isRegisterOpen: false }
-    }
-    showLoginBox() {
-        this.setState({ isLoginOpen: true, isRegisterOpen: false })
-    }
-    showRegisterBox() {
-        this.setState({ isRegisterOpen: true, isLoginOpen: false })
-    }
+const LoginRegister = () => {
+    const [ isLoginOpen, setIsLoginOpen] = useState(true)
+    const [ isRegisterOpen, setIsRegisterOpen] = useState(false)
 
-
-    render() {
-        return (
-            <div className="root-container">
+    const showLoginBox = () => {
+        setIsLoginOpen(true)
+        setIsRegisterOpen(false)
+    }
+    const showRegisterBox = () => {
+        setIsLoginOpen(false)
+        setIsRegisterOpen(true)
+    }
+    return (
+        <div className="root-container">
                 <div className="box-controller">
                     <div 
-                        className={"controller " + (this.state.isLoginOpen ? "selected-controller" : "")}
-                        onClick={this
-                        .showLoginBox
-                        .bind(this)}>
+                        className={"controller " + (isLoginOpen ? "selected-controller" : "")}
+                        onClick={showLoginBox}>
                         Login
                     </div>
                     <div 
-                        className={"controller " + (this.state.isRegisterOpen ? "selected-controller" : "")}
-                        onClick={this
-                        .showRegisterBox
-                        .bind(this)}>
+                        className={"controller " + (isRegisterOpen ? "selected-controller" : "")}
+                        onClick={showRegisterBox}>
                         Register
                     </div>
                 </div>
 
                 <div className="box-container">
-                    {this.state.isLoginOpen && <LoginBox />}
-                    {this.state.isRegisterOpen && <RegisterBox />}
+                    {isLoginOpen && <LoginBox />}
+                    {isRegisterOpen && <RegisterBox />}
                 </div>
             </div>
-        );
-    }
+    );
 }
-
-
-export default LoginRegister
+export default LoginRegister;
